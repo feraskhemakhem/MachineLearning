0add6c663e214227a184447a4487b7a4c3754209
feras.khemakhem <foufoukhemakhem@gmail.com>
2019-04-05 21:42:58 -0500

    svm implementation complete

--- code/solution.py
+++ code/solution.py
@@ -22,6 +22,10 @@ def svm_with_diff_c(train_label, train_data, test_label, test_data):
 	Please keep other parameter options as default.
 	No return value is needed
 	'''
+	c = [0.01, 0.1, 1, 2, 3, 5]
+	for i in c:
+		model = svm_train(train_label, train_data, '-c ' + str(i))
+		svm_predict(test_label, test_data, model)
 
 def svm_with_diff_kernel(train_label, train_data, test_label, test_data):
 	'''
@@ -30,9 +34,12 @@ def svm_with_diff_kernel(train_label, train_data, test_label, test_data):
 	on testing label and data.
 	
 	The kernel  you need to try is listing as follow:
-	1. linear kernel
-	2. polynomial kernel
-	3. radial basis function kernel
+	0. linear kernel
+	1. polynomial kernel
+	2. radial basis function kernel
 	Please keep other parameter options as default.
 	No return value is needed
 	'''
+	for i in range(3):
+		model = svm_train(train_label, train_data, '-t ' + str(i))
+		svm_predict(test_label, test_data, model)
